ebc52c64221b1d1a486b5ef2514fcb59d2e8c1e4
Saeed <omni001@proton.me>
2024-06-30 23:46:23 +0330

    update new modulus

---
+++ test/HeyPay.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {HeyPay} from "../src/HeyPay.sol";
+import {Base64} from "../src/HeyPay.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint() public {
+        _mint(msg.sender, 1000000000);
+    }
+}
+
+contract HeyPayTest is Test {
+    HeyPay public heypay;
+    Token public token1;
+    Token public token2;
+    Token public token3;
+
+    function setUp() public {
+        heypay = new HeyPay();
+        token1 = new Token("TEST1", "T1");
+        token2 = new Token("TEST2", "T2");
+        token3 = new Token("TEST3", "T3");
+    }
+
+    function test_DecodeModulus() public {}
+}
